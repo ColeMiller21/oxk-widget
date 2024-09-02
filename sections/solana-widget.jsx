fc0beb63530cc457bce5eef6e26c8af1f43715a5
cole miller <cole.t.miller@gmail.com>
2024-09-02 11:59:02 -0500

    setting provider dynamically for window. error

--- sections/solana-widget.jsx
+++ sections/solana-widget.jsx
@@ -1,44 +1,48 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   createOkxSwapWidget,
   ProviderType, // Add this import
 } from "@okxweb3/dex-widget";
 
-const provider = window.solana;
 const MOTHER_ADDRESS = "3S8qX1MsMqRbiwKg2cQyx7nis1oHMgaCuc9c4VfvVdPN";
 const SOL_ADDRESS = "So11111111111111111111111111111111111111111";
 
 export function SolanaWidget() {
   const widgetRef = useRef();
-
-  const initialConfig = {
-    params: {
-      providerType: ProviderType.SOLANA, // Use the enum value instead of a string
-      //   tokenPair: {},
-      theme: "dark",
-      chainIds: [501],
-      tokenPair: {
-        fromChain: 501,
-        toChain: 501,
-        fromToken: SOL_ADDRESS,
-        toToken: MOTHER_ADDRESS,
-      },
-    },
-    provider,
-    listeners: [
-      {
-        event: "ON_CONNECT_WALLET",
-        handler: (token, preToken) => {
-          provider.connect();
-        },
-      },
-    ],
-  };
+  const [provider, setProvider] = useState(null);
 
   useEffect(() => {
-    if (widgetRef.current) {
+    setProvider(window.solana);
+  }, []);
+
+  useEffect(() => {
+    if (widgetRef.current && provider) {
+      const initialConfig = {
+        params: {
+          providerType: ProviderType.SOLANA, // Use the enum value instead of a string
+          //   tokenPair: {},
+          theme: "dark",
+          chainIds: [501],
+          tokenPair: {
+            fromChain: 501,
+            toChain: 501,
+            fromToken: SOL_ADDRESS,
+            toToken: MOTHER_ADDRESS,
+          },
+        },
+        provider,
+        listeners: [
+          {
+            event: "ON_CONNECT_WALLET",
+            handler: (token, preToken) => {
+              provider.connect();
+            },
+          },
+        ],
+      };
+
       const widgetHandler = createOkxSwapWidget(
         widgetRef.current,
         initialConfig
@@ -48,7 +52,7 @@ export function SolanaWidget() {
         widgetHandler?.destroy();
       };
     }
-  }, []);
+  }, [provider]);
 
   return <div ref={widgetRef} />;
 }
